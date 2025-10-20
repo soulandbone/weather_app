523e8e666490ff4daa9d9d0ec1a16fc0d0b492df
Gustavo G <soulandbone@gmail.com>
2025-10-20 13:38:49 -0300

    Added Dependency Injection and general setup

---
+++ lib/di.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:weather_app/cubits/weather_cubit.dart';
+import 'package:weather_app/repositories/weather_repository.dart';
+import 'package:weather_app/services/api_service.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton<ApiService>(() => ApiServiceImpl());
+  getIt.registerLazySingleton<WeatherRepository>(
+    () => WeatherRepositoryImpl(getIt()),
+  );
+  getIt.registerFactory<WeatherCubit>(() => WeatherCubit(getIt()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit.dart';
+import 'package:weather_app/di.dart';
 import 'package:weather_app/presentation/screens/home_page.dart';
 import 'package:weather_app/theming/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  setup();
+  runApp(const BlocProviders());
+}
+
+class BlocProviders extends StatelessWidget {
+  const BlocProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => getIt<WeatherCubit>())],
+      child: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(theme: AppTheme.dark, home: HomePage());
